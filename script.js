9ab5b02e86bfd1a6ffe23b60568b8de30c4b5752
Ekaterina <114247065+AndreevaEkaterina@users.noreply.github.com>
2023-03-26 00:25:43 +0800

    сделала ДЗ_3

--- script.js
+++ script.js
@@ -1,33 +1,55 @@
 // задача 1
-const num1 = +prompt("Введите первое число: ");
-const num2 = +prompt("Введите второе число: ");
-(num1 === 1 && num2 === 3) ||
-(num1 < 1 && num2 > 3) ||
-(num1 === 1 && num2 > 3) ||
-(num1 < 1 && num2 === 3)
-  ? alert("Условие выполняется!")
-  : alert("Условие не выполняется.");
+let userNum = +prompt("Введите число: ");
+let power1 = 3;
+let cubeNum = (num, power) => {
+  return Math.pow(num, power);
+};
+alert(`Число ${userNum} в кубе равно: ${cubeNum(userNum, power1)}`);
 // задача 1
 // задача 2
-let test = confirm('"Ок" - true, "Отмена" - false.');
-test === true ? alert("+++") : alert("---");
+let salary = +prompt("Введите заработную плату без учета налогов: ");
+function NDS(salary) {
+  if (salary === Number.isNaN(NaN)) {
+    alert(`Введите число!`);
+  } else {
+    salary = salary * 0.87;
+  }
+  return salary;
+}
+alert(`Заработная плата с учетом налогов равна: ${NDS(salary)}.`);
 // задача 2
 // задача 3
-let day = +prompt("Введите число месяца: ");
-if (day < 1 || day > 31) {
-  alert("Введите число от 1 до 31");
-} else if (day < 11) {
-  alert("Число находится в первой декаде.");
-} else if (day > 10 && day < 21) {
-  alert("Число находится во второй декаде.");
-} else {
-  alert("Число находится в третьей декаде.");
-}
+let num1 = +prompt("Введите первое число: ");
+let num2 = +prompt("Введите второе число: ");
+let num3 = +prompt("Введите третье число: ");
+let maxNum = (a, b, с) => {
+  return Math.max(a, b, с);
+};
+alert(`Максимальное число: ${maxNum(num1, num2, num3)}`);
 // задача 3
 // задача 4
-const num3 = +prompt("Введите число: ");
-const units = num3 % 10;
-const ten = Math.trunc(num3 / 10) % 10;
-const hundreds = Math.trunc(num3 / 100) % 10;
-alert(`В числе ${num3} ${hundreds} сотен, ${ten} десятков, ${units} единиц.`)
+let num5 = +prompt("Введите первое число: ");
+let num6 = +prompt("Введите второе число: ");
+let sum = (a, b) => {
+  return a + b;
+};
+function diff(a, b) {
+  if (a > b) {
+    return a - b;
+  } else if (a < b) {
+    return b - a;
+  } else {
+    return 0;
+  }
+}
+let mult = (a, b) => {
+  return a * b;
+};
+let div = (a, b) => {
+  return a / b;
+};
+alert(`Сумма чисел равна: ${sum(num5, num6)}`);
+alert(`Разность чисел равна: ${diff(num5, num6)}`);
+alert(`Произведение чисел равно: ${mult(num5, num6)}`);
+alert(`Частное чисел равна: ${div(num5, num6)}`);
 // задача 4
